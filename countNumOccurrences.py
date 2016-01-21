c9fb502b766817a56d67de19e2b5536e1def0c07
Shidan Xu <shidanxu@mit.edu>
2016-01-21 05:05:31 -0500

    fix

--- countNumOccurrences.py
+++ countNumOccurrences.py
@@ -8,14 +8,16 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-	currentPath = os.getcwd() + "/../../"
-	oldpath = "../../alllogs"
+	currentPath = os.getcwd()
+	oldpath = "../../alllogs/"
 
 	files = utilities.get_all_files(oldpath)
+
+	current_path = currentPath + oldpath
 	
 	for afile in files:
 		print afile
-		with open(os.path.join(currentPath, afile), 'r') as f:
+		with open(os.path.join(current_path, afile), 'r') as f:
 			content = f.readlines()
 			count = 0
 			for line in content:
