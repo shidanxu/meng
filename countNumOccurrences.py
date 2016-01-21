5b11b14e72cbe5b9ba533397e31ecab023d5da05
Shidan Xu <shidanxu@mit.edu>
2016-01-21 05:08:54 -0500

    fix

--- countNumOccurrences.py
+++ countNumOccurrences.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
 	for afile in files:
 		print afile
 		for individual_files in os.path.join(current_path, afile):
+			print individual_files
 			with open(os.path.join(os.path.join(current_path, afile), individual_files), 'r') as f:
 				content = f.readlines()
 				count = 0
