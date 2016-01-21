17b85c4ba6f0e50c52189cc35915cf8d6ecb7808
Shidan Xu <shidanxu@mit.edu>
2016-01-21 05:12:38 -0500

    fix

--- countNumOccurrences.py
+++ countNumOccurrences.py
@@ -14,14 +14,17 @@ if __name__ == '__main__':
 	files = utilities.get_all_files(oldpath)
 
 	current_path = os.path.join(currentPath, oldpath)
-	
+	count_list = []
+
 	for afile in files:
 		print afile
 		for individual_files in os.listdir(os.path.join(current_path, afile)):
-			print individual_files
+			# print individual_files
 			with open(os.path.join(os.path.join(current_path, afile), individual_files), 'r') as f:
 				content = f.readlines()
-				count = 0
-				for line in content:
-					count += 1
-				print count
+				count = len(content)
+				# print count
+				if count > 300:
+					print content
+					raw_input()
+				count_list.append(count)
